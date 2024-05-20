dc17fa0baab0dbc3fcb80371f6d727edc1465317
km-saifullah <kmsaifullah16@gmail.com>
2024-05-20 19:24:25 +0600

    music app done

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import React, {useState, useEffect} from 'react';
+import {
+  ActivityIndicator,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+import {setupPlayer, addTrack} from '../musicPlayerServices';
+import MusicPlayer from './screens/MusicPlayer';
+
+function App(): React.JSX.Element {
+  const [isPlayerReady, setIsPlayerReady] = useState(false);
+
+  async function setup() {
+    let isSetUp = await setupPlayer();
+    if (isSetUp) {
+      await addTrack();
+    }
+    setIsPlayerReady(isSetUp);
+  }
+
+  useEffect(() => {
+    setup();
+  }, []);
+
+  if (!isPlayerReady) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={'light-content'} />
+      <MusicPlayer />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
